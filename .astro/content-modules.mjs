8e518c93500f8a56742bc407e1c5595d2e588ec8
Jordi <destyfilms@gmail.com>
2025-10-09 12:59:56 +0200

    Fix: Corregir orden y duplicados de blog posts
    - Corregido orden de blogs del 1 al 3 de izquierda a derecha
    - Solucionado problema de duplicados en página de blog (mostraba 3 veces blog-post-3)
    - Cambiado posts[0] por posts[index] para mostrar posts diferentes
    - Simplificado lógica de FeaturedNews para evitar duplicados
    - Agregado Math.min(3, posts.length) para manejar casos con menos de 3 posts

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -2,5 +2,14 @@
 export default new Map([
 ["src/content/blog/ca/hola-mon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fca%2Fhola-mon.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/es/hola-mundo.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fes%2Fhola-mundo.mdx&astroContentModuleFlag=true")],
-["src/content/blog/en/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fen%2Fhello-world.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/en/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fen%2Fhello-world.mdx&astroContentModuleFlag=true")],
+["src/content/blog/es/blog-post-1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fes%2Fblog-post-1.mdx&astroContentModuleFlag=true")],
+["src/content/blog/es/blog-post-2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fes%2Fblog-post-2.mdx&astroContentModuleFlag=true")],
+["src/content/blog/es/blog-post-3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fes%2Fblog-post-3.mdx&astroContentModuleFlag=true")],
+["src/content/blog/ca/blog-post-1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fca%2Fblog-post-1.mdx&astroContentModuleFlag=true")],
+["src/content/blog/ca/blog-post-2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fca%2Fblog-post-2.mdx&astroContentModuleFlag=true")],
+["src/content/blog/ca/blog-post-3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fca%2Fblog-post-3.mdx&astroContentModuleFlag=true")],
+["src/content/blog/en/blog-post-1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fen%2Fblog-post-1.mdx&astroContentModuleFlag=true")],
+["src/content/blog/en/blog-post-2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fen%2Fblog-post-2.mdx&astroContentModuleFlag=true")],
+["src/content/blog/en/blog-post-3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fen%2Fblog-post-3.mdx&astroContentModuleFlag=true")]]);
 		
